8e5d0a279ac48f9540d54b6c2690dba1a34c3b10
Martin Kamleithner <martin.kamleithner@gmail.com>
2015-06-01 11:59:28 +0200

    initial commit

---
+++ src/ss15/cflp/CFLP.java
@@ -0,0 +1,28 @@
+package ss15.cflp;
+
+/**
+ * Klasse zum Berechnen der Lösung mittels Branch-and-Bound.
+ * Hier sollen Sie Ihre Lösung implementieren.
+ */
+public class CFLP extends AbstractCFLP {
+
+	public CFLP(CFLPInstance instance) {
+		// TODO: Hier ist der richtige Platz fuer Initialisierungen
+	}
+
+	/**
+	 * Diese Methode bekommt vom Framework maximal 30 Sekunden Zeit zur
+	 * Verfügung gestellt um eine gültige Lösung
+	 * zu finden.
+	 * 
+	 * <p>
+	 * Fügen Sie hier Ihre Implementierung des Branch-and-Bound Algorithmus
+	 * ein.
+	 * </p>
+	 */
+	@Override
+	public void run() {
+		// TODO: Diese Methode ist von Ihnen zu implementieren
+	}
+
+}
